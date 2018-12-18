9af5344f4320526495e5efed2f0d2ea7f0388608
HumanEquivalentUnit <HumanEquivalentUnit@users.noreply.github.com>
2018-12-18 09:25:34 +0000

    Update 2018-12-01-PowerShell-p1-and-p2.ps1

--- 2018-12-01-PowerShell-p1-and-p2.ps1
+++ 2018-12-01-PowerShell-p1-and-p2.ps1
@@ -1,18 +1,15 @@
+# Code after optimizing runtime from 3.1s to 0.063s
+# Blog post about there is here: https://humanequivalentunit.github.io/AoC-Optimizing-Day-1/
+
 $nums = [int[]][system.io.file]::ReadAllLines('d:\aoc\2018\1\data.txt')
 
 # Part 1
-[System.Linq.Enumerable]::Sum($nums)
+[System.Console]::WriteLine([System.Linq.Enumerable]::Sum($nums))
 
-# Part 2
-$lookup  = [System.Collections.Generic.HashSet[int]]::new()
-$runningSum = 0
-$running = $true
-while ($running)
-{
-    foreach ($n in $nums)
-    {
-        $runningSum += $n
-        if ($lookup.Contains($runningSum)) {$running = $false; $runningSum; break}
-        [void]$lookup.Add($runningSum)
-    }
+# Part 2 - keep summing until a repeat is seen.
+$lookup = [System.Collections.Generic.HashSet[int]]::new()
+$runningTotal = 0
+:outerLoop while ($true) { 
+    foreach ($n in $nums) { $runningTotal += $n; if (-not $lookup.Add($runningTotal)) { break outerLoop } } 
 }
+[System.Console]::WriteLine($runningTotal)
