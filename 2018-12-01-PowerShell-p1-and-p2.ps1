b00e53c4ceee76d7246cbb680342175b0590aec0
HumanEquivalentUnit <HumanEquivalentUnit@users.noreply.github.com>
2018-12-18 08:50:25 +0000

    speed tweaks

--- 2018-12-01-PowerShell-p1-and-p2.ps1
+++ 2018-12-01-PowerShell-p1-and-p2.ps1
@@ -1,16 +1,18 @@
-#
-# Part 1 add the numbers
-#
-get-content .\data.txt |% { [int] $_ } | measure -sum
+$nums = [int[]][system.io.file]::ReadAllLines('d:\aoc\2018\1\data.txt')
 
+# Part 1
+[System.Linq.Enumerable]::Sum($nums)
 
-#
-# Part 2, add the numbers repeatedly until any frequency repeats
-#
-$nums = [int[]](get-content .\data.txt)
-$lookup=@{}
-$current=0
-while ($true) { 
-    $nums.foreach{ $current += $_;   if ($lookup.ContainsKey($current)) {break}; $lookup[$current]++; } 
+# Part 2
+$lookup  = [System.Collections.Generic.HashSet[int]]::new()
+$runningSum = 0
+$running = $true
+while ($running)
+{
+    foreach ($n in $nums)
+    {
+        $runningSum += $n
+        if ($lookup.Contains($runningSum)) {$running = $false; $runningSum; break}
+        [void]$lookup.Add($runningSum)
+    }
 }
-$current
